014e8f67f4f7327cafe2eff45514e44adfa54a48
Henry K <hkewnarny@gmail.com>
2011-06-10 12:48:19 -0700

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates :title, :uniqueness => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :image_url, :format => {
-  	:with => %r{\.(gif|jpg|png)$}i
+  	:with => %r{\.(gif|jpg|png)$}i,
   	:message => 'must be gif, jpg, or png!'
   }
 end
